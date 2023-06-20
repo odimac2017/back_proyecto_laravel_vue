14f40d031ad01808cfee8dc244d28bb263a7b640
Mauro Calle <mauro24odin@yahoo.es>
2023-06-19 21:50:00 -0500

    Proyecto base de Laravel

---
+++ database/migrations/2023_06_20_020340_create_personas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nombres', 50);
+            $table->string('apellidos', 50);
+            $table->string('documento', 20)->nullable();
+            $table->string('direccion', 255)->nullable();
+            $table->string('telefono', 20)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personas');
+    }
+};
